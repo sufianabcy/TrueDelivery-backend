a35323d58687970d1c845a8ff60bf665e0b41600
Sufian Choudhury <sufianabcy@users.noreply.github.com>
2026-04-17 23:08:09 +0530

    Fix OPTIONS handler syntax: remove invalid path parameter

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const corsOptions = configuredOrigins.length > 0 ? {
 } : {};
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options(cors(corsOptions));
 
 app.use(express.json());
 
